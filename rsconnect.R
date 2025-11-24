35cdaf26cef89e2ea6f9dc6b5ded26dbe7cbb3ef
Fernando Rodriguez Alvarado <fernandorodrig@gmail.com>
2025-11-24 12:34:17 +0100

    Actualizar rsconnect.R

--- rsconnect.R
+++ rsconnect.R
@@ -1,5 +1,1 @@
 require(rsconnect)
-
-rsconnect::setAccountInfo(name='jfer2pi',
-                          token='4563D6F94C5551BA0687FC54062D670F',
-                          secret='E4VW8qd+l1ooX7Ep5vE1CPiehLAAzaW1kgDSa/LM')
